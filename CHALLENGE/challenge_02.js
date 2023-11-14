84bb85aff206ae5aaadf45aa22ef5310b449a7ed
lpfalcon <lpfalcon99@gmail.com>
2023-11-14 14:25:47 -0500

    CHALLENGE_02

---
+++ CHALLENGE/challenge_02.js
@@ -0,0 +1,51 @@
+/**
+ * En el mundo del espionaje, se utiliza un lenguaje de codificación con símbolos que realizan operaciones matemáticas simples.
+
+Tu tarea es crear un mini compilador que interprete y ejecute programas escritos en este lenguaje de símbolos.
+
+Las operaciones del lenguaje son las siguientes:
+
+"#" Incrementa el valor numérico en 1.
+"@" Decrementa el valor numérico en 1.
+"*" Multiplica el valor numérico por sí mismo.
+"&" Imprime el valor numérico actual.
+El valor numérico inicial es 0 y las operaciones deben aplicarse en elorden en que aparecen en la cadena de símbolos.
+
+** Ejemplos de entrada: **
+Entrada: "##*&"
+Salida esperada: "4"
+Explicación: Incrementa (1), incrementa (2), multiplica (4), imprime (4).
+
+Entrada: "&##&*&@&"
+Salida esperada: "0243"
+Explicación: Imprime (0), incrementa (1), incrementa (2), imprime (2), multiplica (4), imprime (4), decrementa (3), imprime (3).
+
+** Tu desafío: **
+Desarrolla un mini compilador que tome una cadena de texto y devuelva otra cadena de texto con el resultado de ejecutar el programa.
+ */
+
+export default function compiler(string){
+    let value = 0;
+    const printSymbol = '&'
+    const stringValues = []
+    const stringObjects = {
+        '#': (value)=> (value + 1),
+        '@': (value)=> (value -1),
+        '*': (value)=> (value * value),
+        '&': (value) => value
+    }
+
+    for (let index = 0; index < string.length; index++) {
+        const element = string[index];
+        if(stringObjects[element]){
+            if(printSymbol === element){
+                console.log(value)
+                stringValues.push(value)
+            }
+            value = stringObjects[element](value)
+        }
+        
+    }
+
+    return stringValues.join('')
+}
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // Import stylesheets
-import {countWords, countWords2} from './CHALLENGE_01/index.js'
+import {countWords, countWords2} from './CHALLENGE/challenge_01.js'
+import compiler from './CHALLENGE/challenge_02.js'
 
-const result = countWords('casas casa casasas casa');
-const result2 = countWords2('casas casa casasas casa');
-console.log(result, result2)
+const challenge_01 = countWords('casas casa casasas casa');
+const challenge_01_2 = countWords2('casas casa casasas casa');
+// CHALLENGE_01 RESULTS
+console.log(challenge_01, challenge_01_2)
+
+// CHALLENGE_02 RESULTS
+
+const challenge_02 = compiler('&##&*&@&')
+
+console.log(challenge_02)
